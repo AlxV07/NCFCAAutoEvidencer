d593f797adfd6ffbdac8c221b93a4a6b4c7d752f
alxv05 <alexander.kai.chen@gmail.com>
2024-03-12 13:47:14 -0500

    better alignment; publisher credentials field

--- docs/script.js
+++ docs/script.js
@@ -1,83 +1,34 @@
-function timesNewRomanSpan(text) {
-    return `<span style="font-family: 'Times New Roman', Times, serif">${text}</span>`
-}
-
-function italicizedSpan(text) {
-    return `<span style="font-style: italic">${text}</span>`
-}
-
-function boldSpan(text) {
-    return `<span style="font-weight: bold">${text}</span>`
-}
-
-function underlinedSpan(text) {
-    return `<span style="text-decoration: underline">${text}</span>`
-}
-
-function twelvePtSpan(text) {
-    return `<span style="font-size: 12pt">${text}</span>`
-}
-
-function tenPtSpan(text) {
-    return `<span style="font-size: 10pt;">${text}</span>`
-}
-
-function linkSpan(text) {
-    return `<a href=${text}><span style="color: cornflowerblue" >${
-        italicizedSpan(underlinedSpan(tenPtSpan(text)))
-    }</span></a>`
-}
-
-function evidenceSpan(text) {
-    return `<span>${
-        twelvePtSpan(`[“]${underlinedSpan(text)}[”]`)
-    }</span>`
-}
-
-function credentialSpan(text) {
-    return `<span>${
-        nonImportantSpan(`(${text})`)
-    }</span>`
-}
-
-function articleSpan(text) {
-    return `<span>${
-        nonImportantSpan(`(“${text}”)`)
-    }</span>`
-}
-
-function nonImportantSpan(text) {
-    return `<span>${
-        italicizedSpan(tenPtSpan(text))
-    }</span>`
-}
-
-function importantSpan(text) {
-    return `<span>${
-        underlinedSpan(italicizedSpan(twelvePtSpan(text)))
-    }</span>`
-}
-
-function impactSpan(text) {
-    return `<span>${
-        twelvePtSpan(boldSpan(`MPX: ${text}`))
-    }</span>`
-}
+function timesNewRomanSpan(text) {return `<span style="font-family: 'Times New Roman', Times, serif">${text}</span>`}
+function italicizedSpan(text) {return `<span style="font-style: italic">${text}</span>`}
+function boldSpan(text) {return `<span style="font-weight: bold">${text}</span>`}
+function underlinedSpan(text) {return `<span style="text-decoration: underline">${text}</span>`}
+function twelvePtSpan(text) {return `<span style="font-size: 12pt">${text}</span>`}
+function tenPtSpan(text) {return `<span style="font-size: 10pt;">${text}</span>`}
+function linkSpan(text) {return `<a href=${text}><span style="color: cornflowerblue" >${italicizedSpan(underlinedSpan(tenPtSpan(text)))}</span></a>`}
+function evidenceSpan(text) {return `<span>${twelvePtSpan(`[“]${underlinedSpan(text)}[”]`)}</span>`}
+function credentialSpan(text) {return `<span>${nonImportantSpan(`(${text})`)}</span>`}
+function articleSpan(text) {return `<span>${nonImportantSpan(`(“${text}”)`)}</span>`}
+function nonImportantSpan(text) {return `<span>${italicizedSpan(tenPtSpan(text))}</span>`}
+function importantSpan(text) {return `<span>${underlinedSpan(italicizedSpan(twelvePtSpan(text)))}</span>`}
+function impactSpan(text) {return `<span>${twelvePtSpan(boldSpan(`MPX: ${text}`))}</span>`}
 
 function updateFormattedText() {
     document.getElementById('copyButton').textContent = 'Copy'
     const formattedTextDiv = document.getElementById('formattedEvidence');
     const author = importantSpan(document.getElementById('authorInput').textContent)
-    const credentials = credentialSpan(document.getElementById('authorCredentialsInput').textContent)
+    const authorCredentials = credentialSpan(document.getElementById('authorCredentialsInput').textContent)
     const accessed = importantSpan(document.getElementById("accessedDate").textContent)
     const published = importantSpan(document.getElementById('publishedDate').textContent)
     const publisher = importantSpan(document.getElementById('publisherInput').textContent)
+    const publisherCredentials = credentialSpan(document.getElementById('publisherCredentialsInput').textContent)
     const article = articleSpan(document.getElementById('articleInput').textContent)
     const link = linkSpan(document.getElementById('linkInput').textContent)
     const evd = evidenceSpan(document.getElementById('evidenceInput').textContent)
     const impact = impactSpan(document.getElementById('impactInput').textContent)
-    const citing = nonImportantSpan(`According to ${author} ${credentials}, Accessed on ${accessed}, Published on ${published} by ${publisher}, ${article} ${link}`)
-    formattedTextDiv.innerHTML = timesNewRomanSpan(`${citing}<br>${evd}<br>${impact}`)
+    const citation = nonImportantSpan(
+        `According to ${author} ${authorCredentials}, Accessed on ${accessed}, Published on ${published} by ${publisher} ${publisherCredentials}, ${article} ${link}`
+    )
+    formattedTextDiv.innerHTML = timesNewRomanSpan(`${citation}<br>${evd}<br>${impact}`)
 }
 
 function setDate(component) {
@@ -105,6 +56,7 @@ function clearForm() {
     document.getElementById("accessedDate").textContent = ''
     document.getElementById('publishedDate').textContent = ''
     document.getElementById('publisherInput').textContent = ''
+    document.getElementById('publisherCredentialsInput').textContent = ''
     document.getElementById('articleInput').textContent = ''
     document.getElementById('linkInput').textContent = ''
     document.getElementById('evidenceInput').textContent = ''
@@ -118,6 +70,7 @@ function addListeners() {
     document.getElementById("accessedDate").addEventListener('input', updateFormattedText)
     document.getElementById('publishedDate').addEventListener('input', updateFormattedText)
     document.getElementById('publisherInput').addEventListener('input', updateFormattedText)
+    document.getElementById('publisherCredentialsInput').addEventListener('input', updateFormattedText)
     document.getElementById('articleInput').addEventListener('input', updateFormattedText)
     document.getElementById('linkInput').addEventListener('input', updateFormattedText)
     document.getElementById('evidenceInput').addEventListener('input', updateFormattedText)
