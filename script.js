44100d2736998f97443a232c842bf72a1ab1d709
alxv05 <alexander.kai.chen@gmail.com>
2024-11-03 20:06:47 -0600

    simple auto publisher credentialing for known websites

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ To be implemented soon.<br><br>
 NOTICE: Massive rework of application in progress.  Please report any bugs!<br><br>
 
 Settings will include: <br>
-- Access to auto-citation database. <br>
+- Control auto-citation tools. <br>
 - Color theme customization. <br>`;
 
 const aboutContent = `
 <h2>About</h2>
 
-NCFCAAutoEvidencer Version: 3.0.1.3<br>
+NCFCAAutoEvidencer Version: 3.1.2<br>
 Last updated: 11/3/24<br><br>
 
 A tool to quickly format evidence for NCFCA Debate.
 Copy-and-paste evidence straight from sources to automatically format cards.
 <br><br>
 
-NCFCAAutoEvidencer Version 3+ introduces new usability, wrapping the good old Evidencer in a multi-tab database-powered application.<br>
+NCFCAAutoEvidencer Version 3+ introduces new usability, wrapping the good old AutoEvidencer in a multi-tab database-powered application.<br>
 Version 3+ will enable users to:<br>
 - Automatically cite from a growing database of citations of known authors, publishers, and websites.<br>
 - Save & edit multiple pieces of evidence across sessions and devices with tabs.<br>
@@ -375,7 +375,7 @@ function addContentListeners() {
 
 // === Evidencing ===
 
-function timesNewRomanSpan(text) {return `<span style="font-family: 'Times New Roman', Times, serif">${text}</span>`}
+function timesNewRomanSpan(text) {return `<span style="font-family: 'Times New Roman', Times, serif; font-style: normal; text-decoration: none; font-weight: normal;">${text}</span>`}
 function italicizedSpan(text) {return `<span style="font-style: italic">${text}</span>`}
 function boldSpan(text) {return `<span style="font-weight: bold">${text}</span>`}
 function underlinedSpan(text) {return `<span style="text-decoration: underline">${text}</span>`}
@@ -474,4 +474,136 @@ function updateFormattedText() {
     formattedDisplay.innerHTML = timesNewRomanSpan(`${citation}`)
     updateCurrentTabContent()
     updateCookies()
+
+    if (fillPublisherCredentials) {
+        const l = document.getElementById('input_link').textContent
+        if (l !== prevLink) {
+            prevLink = l;
+            if (prevLink !== '') {
+                try {
+                    const p = urlStringToPublisher(prevLink);
+                    if (p !== null) {
+                        fillCredentialsFromPublisher(p)
+                    }
+                } catch (e) { }
+            }
+        }
+    }
 }
+
+
+// === Citation Completion ===
+
+let prevLink = '';
+let fillPublisherCredentials = true;
+
+function urlStringToPublisher(urlString) {
+    let hostname = new URL(urlString).hostname;
+    let parts = hostname.split(".");
+    for (let i = 0; i < parts.length; i++) {
+        if (publisherToName.has(parts[i])) {
+            return parts[i];
+        }
+    }
+    return null;
+}
+
+function fillCredentialsFromPublisher(p) {
+    if (publisherToName.has(p)) {
+        document.getElementById('input_publisher').textContent = publisherToName.get(p)
+    }
+    if (publisherToCredential.has(p)) {
+        document.getElementById('input_publisherCredentials').textContent = publisherToCredential.get(p)
+    }
+    updateFormattedText()
+}
+
+const publisherToName = new Map([
+    ['csis', 'Center for Strategic and International Studies'],
+    ['apnews', 'Associated Press News'],
+    ['cnn', 'CNN'],
+    ['fpri', 'Foreign Policy Research Institute'],
+    ['borgenproject', 'The Borgen Project'],
+    ['npr', 'NPR'],
+    ['usaid', 'USAID'],
+    ['voanews', 'VOA News'],
+    ['crsreports', 'Congressional Research Service'],
+    ['state', 'U.S. Department of State'],
+    ['foreignpolicy', 'Foreign Policy News'],
+    ['reuters', 'Reuters'],
+    ['cbsnews', 'CBS News'],
+    ['ustr', 'Office of the United States Trade Representative'],
+])
+
+const publisherToCredential = new Map([
+    ['csis',
+        'The Center for Strategic and International Studies (CSIS) is a bipartisan, nonprofit policy research ' +
+        'organization dedicated to advancing practical ideas to address the world’s greatest challenges. CSIS is ' +
+        'regularly called upon by Congress, the executive branch, the media, and others to explain the day’s events ' +
+        'and offer recommendations to improve U.S. strategy.'
+    ],
+    ['apnews',
+        'Associated Press News is one of the most trusted sources of fast, accurate, unbiased news in all formats ' +
+        'and the essential provider of the technology and services vital to the news business.'
+    ],
+    ['cnn',
+        'CNN Worldwide is the most honored brand in cable news, reaching more individuals on television and online ' +
+        'than any other cable news organization in the United States. Globally, people across the world can watch CNN' +
+        ' International, which is widely distributed in over 200 countries and territories. CNN Digital is the #1 ' +
+        'online news destination, with more unique visitors than any other news source.'
+    ],
+    ['fpri',
+        'The Foreign Policy Research Institute (FPRI) is a nonpartisan Philadelphia-based think tank dedicated to ' +
+        'strengthening US national security and improving American foreign policy.'
+    ],
+    ['borgenproject',
+        'The Borgen Project is a nonprofit organization that is addressing poverty and hunger and working towards ' +
+        'ending them. We meet with U.S. leaders to build support for life-saving legislation and effective ' +
+        'poverty-reduction programs.'
+    ],
+    ['npr',
+        'NPR is an independent, nonprofit media organization that was founded on a mission to create a more informed ' +
+        'public. Every day, NPR connects with millions of Americans on the air, online, and in person to explore the ' +
+        'news and ideas. Through its network of member stations, NPR makes local stories national, national stories ' +
+        'local, and global stories personal.'
+    ],
+    ['usaid',
+        'USAID leads international development and humanitarian efforts to save lives, reduce poverty, strengthen ' +
+        'democratic governance and help people progress beyond assistance. President John. F. Kennedy created the ' +
+        'United States Agency for International Development by executive order in 1961 to lead the US government’s' +
+        ' international development and humanitarian efforts. '
+    ],
+    ['voanews',
+        'Voice of America (VOA) is the largest U.S. international broadcaster, providing news and information ' +
+        'in nearly 50 languages to an estimated weekly audience of more than 354 million people. VOA produces content ' +
+        'for digital, television, and radio platforms. '
+    ],
+    ['crsreports',
+        'The Congressional Research Service is a public policy research institute of the United States Congress. ' +
+        'Operating within the Library of Congress, it works primarily and directly for members of Congress and their ' +
+        'committees and staff on a confidential, nonpartisan basis.'
+    ],
+    ['foreignpolicy',
+        'Foreign Policy is an American news publication founded in 1970 focused on global affairs, current events, ' +
+        'and domestic and international policy. It produces content daily on its website and app, and in four ' +
+        'print issues annually.'
+    ],
+    ['wola',
+        'The Washington Office on Latin America is a United States non-governmental organization whose stated goal ' +
+        'is to promote human rights, democracy, and social and economic justice in Latin America and the Caribbean.'
+    ],
+    ['reuters',
+        'Reuters is a news agency which employs around 2,500 journalists and 600 photojournalists in about 200 locations ' +
+        'worldwide writing in 16 languages. Reuters is one of the largest and most trusted news agencies in the world.'
+    ],
+    ['cbsnews',
+        'CBS News is the news division of the American television and radio broadcaster CBS.'
+    ],
+    ['ustr',
+        'The Office of the United States Trade Representative (USTR) is an agency of more than 200 committed ' +
+        'professionals with decades of specialized experience in trade issues and regions of the world. We ' +
+        'negotiate directly with foreign governments to create trade agreements, to resolve disputes, and to ' +
+        'participate in global trade policy organizations. '
+    ]
+]);
+
