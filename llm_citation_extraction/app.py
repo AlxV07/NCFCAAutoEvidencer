fb04d74c7ec677110e67ec028cd0829d0691d030
alxv05 <alexander.kai.chen@gmail.com>
2024-12-04 07:53:59 -0600

    update app playing

--- llm_citation_extraction/app.py
+++ llm_citation_extraction/app.py
@@ -1,7 +1,9 @@
+import time
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from g4f.client import Client
-
+from gpt4free_main.g4f.client import Client
+from gpt4free_main.g4f import Provider
 
 app = Flask(__name__)
 CORS(app)
@@ -9,9 +11,13 @@ client = Client()
 
 
 def generate_response(prompt: str):
-    c = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
-    {"role": "system", "content": "Find the author of this article and the link to his bio page from this HTML. Return the author's name and the link, separated by a semi-colon and no extra whitespace."},
-    {"role": "user", "content": prompt}
+    # provider = Provider.AiChats,
+    c = client.chat.completions.create(model="gpt-3.5-turbo", provider=Provider.Allyfy, messages=[
+        {"role": "system", "content": "Find the author of this article and the link to his bio page from this HTML. "
+                                      "Return the author's name and the link, separated by a semi-colon and no extra "
+                                      "whitespace or new lines. If you cannot find one of the fields, return null for "
+                                      "that field. Respond only in this format."},
+        {"role": "user", "content": prompt}
     ])
     return c.choices[0].message.content
 
@@ -22,10 +28,29 @@ def process_text():
     data = request.json
     text = data.get('text', '')
 
-    processed_text = generate_response(text)
+    print('received:', text)
+
+    res = []
+    while len(text):
+        t = text[:10000]
+        text = text[10000:]
+
+        print('t = ', t)
+        print('generating, remaining text length:', len(text))
+        print('\n'.join(res))
+
+        r = generate_response(t)
+        res.append(r)
+
+        p = r.split(';')
+        if len(p) == 2 and p[0] != 'null':
+            break
+
+        time.sleep(1)
 
     # Return the processed text as a JSON response
-    return jsonify({'processed_text': processed_text})
+    print('finished; responding')
+    return jsonify({'content': res[-1]})
 
 
 if __name__ == '__main__':
