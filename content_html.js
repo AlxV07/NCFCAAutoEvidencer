fe9f18cbaf88bcfa14273557c6fa7cbf0313d851
AlxV07 <alexander.kai.chen@gmail.com>
2026-09-15 23:19:02 -0400

    v5.0: add about the builder msg on random tab numbering

--- content_html.js
+++ content_html.js
@@ -52,6 +52,10 @@ overflow: hidden;">
                     </ul>
                 </div>
             </div>
+
+						<div>
+							Why use random numbers for tab naming? Honestly no clue why I did years ago when I first built this thing lol (psst, back then we didn't have AI... I know, crazy right? Coding by hand... good ol' days :_) ). Well, want me to change it?  Maybe if you ask, I might ;)
+						</div>
        </div>
         
        <div class="home-panel" style="font-size: 12px;">
